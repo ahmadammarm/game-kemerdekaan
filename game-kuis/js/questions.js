6b992287a32ee020f6b5c6f389636f5cdb58df92
Ahmad Ammar <ammarmusyaffa11@gmail.com>
2023-08-12 09:34:42 +0700

    update

---
+++ game-kuis/js/questions.js
@@ -0,0 +1,14 @@
+// Creating an Array and passing the number, questions, and answers
+let questions = [
+    {   
+        number: 1,
+        question: "Siapakah Bapak Proklamator Republik Indonesia ?",
+        answers: "Ir. Soekarno",
+        options: [
+            "Ir. Soekarno",
+            "Mohammad Hatta",
+            "Mohammad Yamin",
+            "WR. Soepratman"
+        ]
+    }
+]
